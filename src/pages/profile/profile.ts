bf0e4181ba37810253fa8b4a864f21e9adfafbe4
Willian Rodrigues <oredstonebr@gmail.com>
2018-06-17 21:43:04 -0300

    Adicionado novos inputs na pagina profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -41,9 +41,30 @@ export class ProfilePage {
   expandItem(item) {
     if (item.title === "EMAIL") {
       this.formEmail = this.formBuilder.group({
-        oldMail: ["", Validators.required],
-        newMail: ["", Validators.required],
-        newMailConfirm: ["", Validators.required]
+        oldMail: ["", Validators.required, Validators.email],
+        newMail: ["", Validators.required, Validators.email],
+        confirmMail: ["", Validators.required, Validators.email]
+      });
+    } else if (item.title === "TELEFONE") {
+      this.formTelefone = this.formBuilder.group({
+        oldPhone: ["", Validators.required],
+        newPhone: ["", Validators.required],
+        confirmPhone: ["", Validators.required]
+      });
+    } else if (item.title === "SENHA") {
+      this.formSenha = this.formBuilder.group({
+        oldPassword: ["", Validators.required],
+        newPassword: ["", Validators.required],
+        confirmPassword: ["", Validators.required]
+      });
+    } else if (item.title === "ENDEREÇO") {
+      this.formEndereco = this.formBuilder.group({
+        addressCep: ["", Validators.required],
+        address: ["", Validators.required],
+        addressNumber: ["", Validators.required],
+        addressComplement: ["", Validators.required],
+        addressCity: ["", Validators.required],
+        addressState: ["", Validators.required]
       });
     }
     this.items.map(listItem => {
